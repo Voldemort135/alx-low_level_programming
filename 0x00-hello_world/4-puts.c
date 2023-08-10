17f376f7b6007a360ed948eb35c84d7edf27955b
Voldemort135 <cleve.davey@gmail.com>
2023-08-10 21:55:21 +0200

    C-Hello_World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/*
+ * main - prints a phrase
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	char phrase[] = "Programming is like building a multilingual puzzle";
+	char phrase[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(phrase);
 	return (0);
