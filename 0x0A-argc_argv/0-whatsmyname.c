a54b34c5ec2a656f2ad326086b58088ae60561b4
Voldemort135 <cleve.davey@gmail.com>
2023-09-06 19:14:41 +0200

    C - argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
  * main - prints the name of the program that called it
  * @argc: argument count
  * @argv: argument vector
- * return: always 0
+ * 
+ * Return: always 0
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 
